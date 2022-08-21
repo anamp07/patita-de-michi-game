c71b5e61dead702c1711909721044e3d5793e2de
Shiny <luisluna.am@gmail.com>
2022-08-20 21:27:42 -0500

    Player can move uwu

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+    [SerializeField] float speed = 3;
+
+    Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        moveDirection.x = h;
+        moveDirection.y = v;
+        transform.position += (Vector3)moveDirection * Time.deltaTime * speed;
+    }
+}
