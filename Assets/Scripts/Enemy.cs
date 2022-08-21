a68f3f9e47aed11418198863855f9ba98a9b2837
Shiny <luisluna.am@gmail.com>
2022-08-21 09:43:55 -0500

    Vertical enemy fuincionando

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,19 +9,22 @@ public class Enemy : MonoBehaviour
     const string RIGHT = "right";
     const string UP = "up";
     const string DOWN = "down";
-    
+
     [SerializeField] Transform castPos;
     [SerializeField] float baseCastDistance;
     Rigidbody2D body2D;
     [SerializeField] float speed = 5;
-    string facingDirection;
+    [SerializeField] string facingDirection;
     Vector3 baseScale;
     [SerializeField] bool upDown = false;
     // Start is called before the first frame update
     void Start()
     {
         baseScale = transform.localScale;
-        facingDirection = RIGHT;
+        if (upDown)
+            facingDirection = UP;
+        else
+            facingDirection = RIGHT;
         body2D = GetComponent<Rigidbody2D>();
     }
 
@@ -35,11 +38,13 @@ public class Enemy : MonoBehaviour
     {
         if (upDown)
         {
-            facingDirection = UP;
+            print(facingDirection);
             float velocityY = speed;
 
             if (facingDirection == DOWN)
                 velocityY = -speed;
+
+            print(velocityY);
 
             body2D.velocity = new Vector2(body2D.velocity.x, velocityY);
         }
@@ -63,24 +68,36 @@ public class Enemy : MonoBehaviour
             {
                 ChangeFacingDirection(LEFT);
             }
+            
+            if (facingDirection == UP)
+            {
+                print("entras?");
+                ChangeFacingDirection(DOWN);
+            }else if(facingDirection == DOWN)
+            {
+                print("hitted a wall down");
+                ChangeFacingDirection(UP);
+            }
         }
     }
 
     void ChangeFacingDirection(string newDirection)
     {
         Vector3 newScale = baseScale;
-        if(newDirection == LEFT)
+
+        if (newDirection == LEFT)
             newScale.x = -baseScale.x;
-        else if(newDirection == RIGHT)
+        else if (newDirection == RIGHT)
             newScale.x = baseScale.x;
-        else if(newDirection == DOWN)
-            newScale.y = -baseScale.y;
-        else if(newDirection == UP)
+        else if (newDirection == UP)
             newScale.y = baseScale.y;
+        else if (newDirection == DOWN)
+            newScale.y = -baseScale.y;
 
         transform.localScale = newScale;
 
         facingDirection = newDirection;
+        print("this is the new direction " + facingDirection);
     }
 
     bool IsHittingWall()
@@ -88,13 +105,16 @@ public class Enemy : MonoBehaviour
         bool hit = false;
         float castDistance = baseCastDistance;
         //Define cast distance for left an right
-        if(facingDirection == LEFT)
+        if(facingDirection == LEFT || facingDirection == DOWN)
         {
             castDistance = -baseCastDistance;
         }
 
         Vector3 targetPosition = castPos.position;
-        targetPosition.x += castDistance;
+        if(upDown)
+            targetPosition.y += castDistance;
+        else
+            targetPosition.x += castDistance;
 
         Debug.DrawLine(castPos.position, targetPosition, Color.magenta);
 
